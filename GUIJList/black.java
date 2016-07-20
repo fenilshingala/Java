719ad0b211239ad46cb83210f960370a41dc4ae6
fenilshingala <shingala.fenil@gmail.com>
2016-07-20 23:31:13 +0530

    Create black.java

---
+++ GUIJList/black.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class black {
+	
+	public static void main(String[] args){
+		panther obj = new panther();
+		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		obj.setSize(500,500);
+		obj.setVisible(true);
+	}
+
+}
