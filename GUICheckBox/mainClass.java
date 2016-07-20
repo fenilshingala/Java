e8fcafa617efd801db408c84aed3d257616e7939
fenilshingala <shingala.fenil@gmail.com>
2016-07-20 23:28:12 +0530

    Add files via upload

---
+++ GUICheckBox/mainClass.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class mainClass {
+	
+	public static void main(String[] args){
+		gui obj = new gui();
+		 obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		 obj.setSize(300,300);
+		 obj.setVisible(true);
+	}
+	
+}
