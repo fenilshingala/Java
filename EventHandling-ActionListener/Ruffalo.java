338afc38431b0b14d5aa725c4b61c8af00f45535
fenilshingala <shingala.fenil@gmail.com>
2016-07-20 23:24:30 +0530

    Add files via upload

---
+++ EventHandling-ActionListener/Ruffalo.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+public class Ruffalo {
+	public static void main(String[] args){
+		Mark obj = new Mark();
+		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		obj.setSize(500, 300);
+		obj.setVisible(true);
+	}
+}
