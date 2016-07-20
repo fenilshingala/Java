ff289ec2b0336fc8ef184285ac4e44d51a9c8f07
fenilshingala <shingala.fenil@gmail.com>
2016-07-20 23:48:30 +0530

    Create HomeClass.java

---
+++ Slider/HomeClass.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class HomeClass {
+	public static void main(String[] args){
+		
+		WindowFrame obj = new WindowFrame();
+		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		obj.setSize(300, 300);
+		obj.setVisible(true);
+		
+	}
+}
