81632917602c629fbda574791b0bd023221c9601
fenilshingala <shingala.fenil@gmail.com>
2016-07-20 23:37:17 +0530

    Create MainClass.java

---
+++ JColorChooser/MainClass.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class MainClass {
+	public static void main(String[] args){
+		
+		Nothing obj = new Nothing();
+		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	}
+}
