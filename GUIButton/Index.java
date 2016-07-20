954e9fa22be97094ead0ffbc20b0ee36708a272b
fenilshingala <shingala.fenil@gmail.com>
2016-07-20 23:27:01 +0530

    Add files via upload

---
+++ GUIButton/Index.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+class Index {
+	public static void main(String[] args){
+		Button i = new Button();
+		i.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		i.setSize(500,500);
+		i.setVisible(true);
+	}
+}
