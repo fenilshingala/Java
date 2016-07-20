4dda4a79000ff3c8558b012ab11f7691096dffef
fenilshingala <shingala.fenil@gmail.com>
2016-07-20 23:35:41 +0530

    Add files via upload

---
+++ Layout/me.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class me extends JFrame{
+	
+	public static void main(String[] args){
+		LayoutClass lo = new LayoutClass();
+		lo.setSize(300,300);
+		lo.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		lo.setVisible(true);
+	}
+
+}
