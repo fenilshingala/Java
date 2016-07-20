43f660a3c4fc144bbee020515bdf898528831cdd
fenilshingala <shingala.fenil@gmail.com>
2016-07-20 23:03:07 +0530

    Create Swift.java

---
+++ hello/Swift.java
@@ -0,0 +1,12 @@
+package hello;
+
+public class Swift {
+	
+	public void function(){
+		System.out.println("Another Class!");
+	}
+	public void hellofun(String str){
+		System.out.println("Hello " + str);
+	}
+	
+}
