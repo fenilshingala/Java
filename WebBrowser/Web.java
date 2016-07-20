0e49472c2adef628b3dc12d7d0620b21015a2205
fenilshingala <shingala.fenil@gmail.com>
2016-07-20 23:55:16 +0530

    Add files via upload

---
+++ WebBrowser/Web.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+public class Web{
+	public static void main(String[] args){
+
+		Browser obj = new Browser();
+		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	}
+}
