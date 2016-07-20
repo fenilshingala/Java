cf5e8795c881f1b9a031e3eb350b66a68791136e
fenilshingala <shingala.fenil@gmail.com>
2016-07-20 23:32:17 +0530

    Add files via upload

---
+++ GUImultiSelectionList/joker.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+public class joker{
+	public static void main(String[] args){
+		gui obj = new gui();
+		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		obj.setSize(300,300);
+		obj.setVisible(true);
+	}
+}
